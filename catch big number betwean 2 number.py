9888c77a865a0439a5d8ecd1c96bd9a04bb30089
Parsa Abyar <117229743+parsaHBK@users.noreply.github.com>
2023-02-25 02:35:22 +0330

    Add files via upload

---
+++ catch big number betwean 2 number.py
@@ -0,0 +1,9 @@
+number_1 = (int(input("use one number: ")))
+number_2 = (int(input("use secend number: ")))
+
+if (number_1>number_2):
+    print("your big number is:",number_1)
+elif (number_2>number_1):
+    print("your big number is:",number_2)
+else:
+    print("your numbers is equal!!")
---
+++ catch next multi 10.py
@@ -0,0 +1,4 @@
+number = (int(input("give number to catch next 10 multi: ")))
+negative = int(number % 10)
+number = number - negative
+print("your ansewr is : ",number+10)

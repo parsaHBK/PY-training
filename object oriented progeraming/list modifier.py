e7cd907fc5465ec4043147ac8d16318ead602f30
Parsa Abyar <117229743+parsaHBK@users.noreply.github.com>
2023-05-14 19:13:56 +0330

    Add files via upload

---
+++ object oriented progeraming/list modifier.py
@@ -0,0 +1,56 @@
+class Listmodifier:
+    def __init__(self,lst) -> None:
+        self.lst = lst
+    def __repr__(self) -> str:
+        return f"list object is : {self.lst}"
+    def integer(self):
+        """return a list of integers
+        the object will not change"""
+        temp=[]
+        for i in self.lst:
+            if not isinstance(i,int):
+                continue
+            temp+=[i]
+        return temp
+    def count(self)-> int:
+        """return a number
+        memebrs of the list"""
+        j=0
+        for i in self.lst:
+            j+=1
+        return j
+    def eqaul_numbers(self)-> list:
+        """return a list with eqaul numbers
+        the object will not change"""
+        temp=[]
+        for i in self.lst:
+            if not isinstance(i,int):
+                continue
+            if i % 2 == 0:
+                temp +=[i]
+        return temp
+    def change_member(self,index,member)-> list:
+        """change the index of memebr on list
+        the object will change"""
+        self.lst[index]=member
+        return self.lst
+    def remove_index(self,index)-> list:
+        """remove the elemnt on list with index number
+        the object will change"""
+        temp = []
+        for i in range(0,self.count()):
+            if i == index:
+                continue
+            temp.append(self.lst[i])
+        self.lst = temp
+        return self.lst
+    def clear(self):
+        """remove evry of memebrs list
+        object will empety list"""
+        self.lst=[]
+        return self.lst
+a=Listmodifier([1,2,3,4,313,2])
+print(a.change_member(4,5))
+print(a.count())
+print(a.remove_index(2))
+print(a.clear())
